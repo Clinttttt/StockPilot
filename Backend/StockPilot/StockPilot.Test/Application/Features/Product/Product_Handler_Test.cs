49e444c9b5aad5f497b83c47a426ed1c339d8024
Clinttttt <clintvillanueva82@gmail.com>
2026-08-01 21:01:12 +0800

    Updating ProductHandlerTest

--- Backend/StockPilot/StockPilot.Test/Application/Features/Product/Product_Handler_Test.cs
+++ Backend/StockPilot/StockPilot.Test/Application/Features/Product/Product_Handler_Test.cs
@@ -20,12 +20,30 @@ namespace StockPilot.Test.Application.Features.Product
                     .UseInMemoryDatabase(
             databaseName: Guid.NewGuid().ToString())
         .Options;
+            using var ct = new CancellationTokenSource();
+            CancellationToken cancellationToken = ct.Token;
 
             await using var context = new AppDbContext(options);
             var currentUser = new Mock<ICurrentUserService>();
             
-            var handler = new AddProductCommandHandler(context);
+            var handler = new AddProductCommandHandler(context, currentUser.Object);
 
+            var command = new AddProductCommand(
+                productName: "Macbook m4 max",
+                sku: "mc-m4",
+                CurrentStock: 15,
+                MinimumStock: 10,
+                ImageUrl: "wijjwewfwfee",
+                CostPrice: 300000,
+                Unit: "basta",
+                CategoryId: Guid.Parse("8f2c7b1a-3d44-4e9f-9a6a-2c5e3f8d9b71")
+                );
+
+           await handler.Handle(command, cancellationToken);
+           await context.SaveChangesAsync(cancellationToken);
+
+            Assert.True(cancellationToken.IsCancellationRequested );
+        
         }
     }
 }
